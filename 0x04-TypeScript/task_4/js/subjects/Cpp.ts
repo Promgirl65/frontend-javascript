8b78e2c6258d958e781c58768901ea4cc31f6017
Promgirl65 <olayinkabusolami@gmail.com>
2025-09-25 14:30:09 +0100

    task_4

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,23 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Extend Teacher with C experience (declaration merging)
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingC === 0 || this.teacher.experienceTeachingC === undefined) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
+
---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,23 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Extend Teacher with Java experience
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || this.teacher.experienceTeachingJava === 0 || this.teacher.experienceTeachingJava === undefined) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
+
