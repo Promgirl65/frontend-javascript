6da037d56aebf6dcb30c690301e641a41a85246c
Promgirl65 <olayinkabusolami@gmail.com>
2025-09-25 14:51:37 +0100

    task_4

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -5,12 +5,12 @@
 /// <reference path="./React.ts" />
 
 // Create and export constants for each subject
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
 // Create and export one Teacher object
-export const cTeacher: Subjects.Teacher = {
+export const cTeacher: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
   experienceTeachingC: 10,
@@ -33,4 +33,3 @@ console.log('React');
 react.setTeacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
-
